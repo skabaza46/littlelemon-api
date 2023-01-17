e197b1c8bd59ce317caf94e332d6d6577065cda8
skabaza <skabaza46@gmail.com>
2023-01-16 23:12:58 -0500

    added two new models 'Menu' and 'Booking'.

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -73,13 +73,19 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER' : 'admindjango',
+        'PASSWORD' : 'employee@123!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
     }
-}
 
 
 # Password validation
