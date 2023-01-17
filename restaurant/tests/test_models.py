1e8b42e1d1fc0cf7cd4f74b2cf12e83448e3a576
skabaza <skabaza46@gmail.com>
2023-01-17 03:10:48 -0500

    Updated Unit Tests.

--- restaurant/tests/test_models.py
+++ restaurant/tests/test_models.py
@@ -2,22 +2,28 @@ from datetime import datetime
 from django.test import TestCase
 from django.utils.timezone import make_aware
 from ..models import Booking, Menu
-
+from ..serializer import BookingSerializer, MenuSerializer
 
 class MenuTest(TestCase):
 
     def test_get_item(self):
+        """Test getting a single Menu record."""
 
-        item = Menu.objects.create(title="IceCream", price=5.80, inventory=100)
-        itemstr = item.get_item()
+        item = Menu.objects.create(title="Ice Cream", price=5.80, inventory=100)
+        serialized_item = MenuSerializer(item)
 
-        self.assertEqual(itemstr, "IceCream : 5.8")
+        self.assertEqual(serialized_item.data.get("title"), "Ice Cream")
+        self.assertEqual(serialized_item.data.get("price"), "5.80")
 
 class BookingTest(TestCase):
 
     def test_get_booking(self):
+        """Test getting a single Booking record."""
+
         booking_date = make_aware(datetime.strptime("2023-01-17", "%Y-%m-%d"))
 
         booking = Booking.objects.create(name="Chris King", no_of_guests=4, booking_date=booking_date)
+        serialized_booking = BookingSerializer(booking)
 
-        self.assertEqual(booking.name, "Chris King")
+        self.assertEqual(serialized_booking.data.get("name"), "Chris King")
+        self.assertEqual(serialized_booking.data.get("no_of_guests"), 4)
