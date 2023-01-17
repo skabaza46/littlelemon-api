554149f8b02c4866099da2afc9eafabe6b8cd626
skabaza <skabaza46@gmail.com>
2023-01-17 03:13:20 -0500

    Used black to format the code base correctly.

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,10 +6,11 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
 
+
 class Menu(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
 
     def get_item(self):
-        return f'{self.title} : {str(self.price)}'
+        return f"{self.title} : {str(self.price)}"
